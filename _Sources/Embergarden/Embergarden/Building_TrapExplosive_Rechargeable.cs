7866530d862b7774e51f40a39c04ace99807ea0b
CMDR-Bill-Doors <Billyanghk@qq.com>
2025-01-14 13:49:51 +0000

    rechargeable trap

---
+++ _Sources/Embergarden/Embergarden/Building_TrapExplosive_Rechargeable.cs
@@ -0,0 +1,53 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Embergarden
+{
+    public class Building_TrapExplosive_Rechargeable : Building_TrapExplosive
+    {
+        int countdown;
+
+        protected Effecter progressBarEffecter;
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref countdown, "countdown");
+            base.ExposeData();
+        }
+
+        public override void Tick()
+        {
+            if (countdown > 0)
+            {
+                countdown--;
+
+                if (progressBarEffecter == null)
+                {
+                    progressBarEffecter = EffecterDefOf.ProgressBar.Spawn();
+                }
+                progressBarEffecter.EffectTick(this, TargetInfo.Invalid);
+                MoteProgressBar mote = ((SubEffecter_ProgressBar)progressBarEffecter.children[0]).mote;
+                mote.progress = countdown / def.building.turretBurstCooldownTime;
+                mote.offsetZ = -0.8f;
+            }
+            base.Tick();
+        }
+
+        protected override void SpringSub(Pawn p)
+        {
+            if (countdown <= 0)
+            {
+                var comp = GetComp<CompExplosive>();
+                comp.AddThingsIgnoredByExplosion([this]);
+                if (progressBarEffecter != null)
+                {
+                    progressBarEffecter.Cleanup();
+                    progressBarEffecter = null;
+                }
+                countdown = def.building.turretBurstCooldownTime.SecondsToTicks();
+
+                base.SpringSub(p);
+            }
+        }
+    }
+}
---
+++ _Sources/Embergarden/Embergarden/CompAbilityEffect_SpawnWithFaction.cs
@@ -0,0 +1,54 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+using Verse.Sound;
+
+namespace Embergarden
+{
+    public class CompAbilityEffect_SpawnWithFaction : CompAbilityEffect
+    {
+        public new CompProperties_AbilitySpawnWithFaction Props => (CompProperties_AbilitySpawnWithFaction)props;
+        protected Thing spawnedThing;
+        public override void Apply(LocalTargetInfo target, LocalTargetInfo dest)
+        {
+            base.Apply(target, dest);
+            spawnedThing = ThingMaker.MakeThing(Props.thingDef);
+            GenPlace.TryPlaceThing(spawnedThing, target.Cell, parent.pawn.Map, ThingPlaceMode.Near);
+            spawnedThing.SetFactionDirect(parent.ConstantCaster.Faction);
+            if (Props.sendSkipSignal)
+            {
+                CompAbilityEffect_Teleport.SendSkipUsedSignal(target, parent.pawn);
+            }
+            Props.effecter?.Spawn(target.Cell, parent.ConstantCaster.Map);
+        }
+
+        public override bool AICanTargetNow(LocalTargetInfo target)
+        {
+            return Valid(target);
+        }
+
+        public override bool Valid(LocalTargetInfo target, bool throwMessages = false)
+        {
+            if (target.Cell.Filled(parent.pawn.Map) || (!Props.allowOnBuildings && target.Cell.GetEdifice(parent.pawn.Map) != null))
+            {
+                if (throwMessages)
+                {
+                    Messages.Message("CannotUseAbility".Translate(parent.def.label) + ": " + "AbilityOccupiedCells".Translate(), target.ToTargetInfo(parent.pawn.Map), MessageTypeDefOf.RejectInput, historical: false);
+                }
+                return false;
+            }
+            return true;
+        }
+    }
+
+    public class CompProperties_AbilitySpawnWithFaction : CompProperties_AbilitySpawn
+    {
+        public EffecterDef effecter;
+
+        public CompProperties_AbilitySpawnWithFaction()
+        {
+            compClass = typeof(CompAbilityEffect_SpawnWithFaction);
+        }
+    }
+}
