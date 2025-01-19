363247692d701624517e67648319f4c3d4a39d53
CMDR-Bill-Doors <Billyanghk@qq.com>
2025-01-19 05:53:26 +0000

    call boss without mechanitor fuck tynan

---
+++ _Sources/Embergarden/Embergarden/CompUseEffect_CallBossgroupNoMechanitor.cs
@@ -0,0 +1,13 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Embergarden
+{
+    public class CompUseEffect_CallBossgroupNoMechanitor : CompUseEffect_CallBossgroup
+    {
+        public override AcceptanceReport CanBeUsedBy(Pawn p)
+        {
+            return Props.bossgroupDef.Worker.CanResolve(p);
+        }
+    }
+}
