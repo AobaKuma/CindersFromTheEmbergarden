a6381b6bedfa5905a89458b0dc11088f4dea5773
CMDR-Bill-Doors <Billyanghk@qq.com>
2025-01-19 05:38:14 +0000

    draftable mech

---
+++ _Sources/Embergarden/Embergarden/HarmonyPatch/Pawn_DraftController_Postfix.cs
@@ -0,0 +1,50 @@
+﻿using Verse;
+using HarmonyLib;
+using RimWorld;
+using System.Reflection;
+
+namespace Embergarden
+{
+    [StaticConstructorOnStartup]
+    [HarmonyPatch]
+    static class Pawn_DraftController_Postfix
+    {
+        static MethodBase TargetMethod()
+        {
+            return typeof(Pawn_DraftController).PropertyGetter("ShowDraftGizmo");
+        }
+
+        [HarmonyPostfix]
+        static void Postfix(ref bool __result, Pawn_DraftController __instance)
+        {
+            if (!__result && __instance.pawn.def.HasModExtension<ModExtension_Draftable>())
+            {
+                __result = true;
+            }
+        }
+    }
+
+    [StaticConstructorOnStartup]
+    [HarmonyPatch]
+    static class Pawn_IsColonyMech_Postfix
+    {
+        static MethodBase TargetMethod()
+        {
+            return typeof(Pawn).PropertyGetter("IsColonyMech");
+        }
+
+        [HarmonyPostfix]
+        static void Postfix(ref bool __result, Pawn __instance)
+        {
+            if (!__result && __instance.def.HasModExtension<ModExtension_Draftable>())
+            {
+                __result = true;
+            }
+        }
+    }
+
+    public class ModExtension_Draftable : DefModExtension
+    {
+
+    }
+}
