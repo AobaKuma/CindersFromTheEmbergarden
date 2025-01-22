f5d8fc1f8dcc5d39ea16d66154bf0173e87fc9f5
CMDR-Bill-Doors <Billyanghk@qq.com>
2025-01-22 14:51:10 +0000

    add hediff

---
+++ _Sources/Embergarden/Embergarden/CompAddHediff.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace Embergarden
+{
+    public class CompAddHediff : ThingComp
+    {
+        CompProperties_AddHediff Props => props as CompProperties_AddHediff;
+
+        public override void PostPostMake()
+        {
+            base.PostPostMake();
+        }
+
+        public override void PostSpawnSetup(bool respawningAfterLoad)
+        {
+            base.PostSpawnSetup(respawningAfterLoad);
+        }
+
+        protected void TryRegenHediff()
+        {
+            if (parent is Pawn p)
+            {
+                foreach (var def in Props.hediffDefs)
+                {
+                    if (!p.health?.hediffSet.HasHediff(def) ?? false)
+                    {
+                        p.health.AddHediff(def);
+                    }
+                }
+            }
+        }
+    }
+
+    public class CompProperties_AddHediff : CompProperties
+    {
+        public CompProperties_AddHediff()
+        {
+            compClass = typeof(CompAddHediff);
+        }
+
+        public List<HediffDef> hediffDefs = new List<HediffDef>();
+    }
+}
