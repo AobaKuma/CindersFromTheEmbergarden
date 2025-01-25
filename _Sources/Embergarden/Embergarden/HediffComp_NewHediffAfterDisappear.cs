3e5a4029cdb02f9dcfaa9ffc41a54845131ef674
CMDR-Bill-Doors <Billyanghk@qq.com>
2025-01-25 15:19:10 +0000

    add hediff

---
+++ _Sources/Embergarden/Embergarden/HediffComp_NewHediffAfterDisappear.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace Embergarden
+{
+    public class HediffComp_NewHediffAfterDisappear : HediffComp
+    {
+        HediffCompProperties_NewHediffAfterDisappear Props => props as HediffCompProperties_NewHediffAfterDisappear;
+
+        public override void CompPostPostRemoved()
+        {
+            if (parent.Severity == 0)
+            {
+                Pawn.health.AddHediff(Props.hediffDef);
+            }
+        }
+    }
+
+    public class HediffCompProperties_NewHediffAfterDisappear : HediffCompProperties
+    {
+        public HediffDef hediffDef;
+        public HediffCompProperties_NewHediffAfterDisappear()
+        {
+            compClass = typeof(HediffComp_NewHediffAfterDisappear);
+        }
+    }
+}
