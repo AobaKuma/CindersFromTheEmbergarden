1796938c01892d9d7048af14695d06d805e9365f
CMDR-Bill-Doors <Billyanghk@qq.com>
2025-01-26 14:58:38 +0000

    bossgroup shenanigans

--- _Sources/Embergarden/Embergarden/CompUseEffect_ActivateHediffs.cs
+++ _Sources/Embergarden/Embergarden/CompUseEffect_ActivateHediffs.cs
@@ -10,7 +10,7 @@ namespace Embergarden
             bool flag = false;
             if (p.health.hediffSet.GetHediffComps<HediffComp_LimitedResurrect>().FirstOrFallback(null) is HediffComp_LimitedResurrect resurrect && !resurrect.CanResurrect) flag = true;
             if (p.health.hediffSet.GetHediffComps<HediffComp_Regen>().FirstOrFallback(null) is HediffComp_Regen regen && !regen.Unlocked) flag = true;
-            return flag ? true : "Cinder_NeedResurrectOrRegen";
+            return flag ? true : "Cinder_NeedResurrectOrRegen".Translate();
         }
 
         public override void DoEffect(Pawn usedBy)
