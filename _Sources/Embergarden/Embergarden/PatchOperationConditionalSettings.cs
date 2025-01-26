02692c2dabfd4ce8999c713a4ecdb1e837c8c215
CMDR-Bill-Doors <Billyanghk@qq.com>
2025-01-26 09:24:35 +0000

    settings

---
+++ _Sources/Embergarden/Embergarden/PatchOperationConditionalSettings.cs
@@ -0,0 +1,44 @@
+﻿using System.Xml;
+using Verse;
+
+namespace Embergarden
+{
+    public class PatchOperationConditionalSettings : PatchOperation
+    {
+        private string key;
+
+        private PatchOperation match;
+
+        private PatchOperation nomatch;
+
+        protected override bool ApplyWorker(XmlDocument xml)
+        {
+            if (GetSetting())
+            {
+                if (match != null)
+                {
+                    return match.Apply(xml);
+                }
+            }
+            else if (nomatch != null)
+            {
+                return nomatch.Apply(xml);
+            }
+            if (match == null)
+            {
+                return nomatch != null;
+            }
+            return true;
+        }
+
+        protected bool GetSetting()
+        {
+            var fieldInfo = typeof(Setting).GetField(key);
+            if (fieldInfo != null)
+            {
+                Log.Warning($"Key named {key} is not present in settings.");
+            }
+            return (bool)fieldInfo.GetValue(CinderMod.settings);
+        }
+    }
+}
---
+++ _Sources/Embergarden/Embergarden/Setting.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using UnityEngine;
+using Verse;
+using static RimWorld.Sketch;
+
+namespace Embergarden
+{
+    public class Setting : ModSettings
+    {
+        public bool CommaEyes = false;
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref CommaEyes, "CommaEyes");
+        }
+    }
+
+    public class CinderMod : Mod
+    {
+        public static Setting settings;
+        public static bool CommaEyes => settings.CommaEyes;
+
+        public CinderMod(ModContentPack content) : base(content)
+        {
+            settings = GetSettings<Setting>();
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            base.DoSettingsWindowContents(inRect);
+            Listing_Standard listing_Standard = new Listing_Standard();
+            listing_Standard.ColumnWidth = (inRect.width - 17f) / 2f;
+            listing_Standard.Begin(inRect);
+            Text.Font = GameFont.Small;
+            listing_Standard.GapLine();
+            listing_Standard.CheckboxLabeled("Cinder_CommaEyesLabel".Translate(), ref settings.CommaEyes, "Cinder_CommaEyesDesc".Translate());
+            listing_Standard.End();
+        }
+    }
+}
