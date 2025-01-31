b3387737b5c71bfaba6715e36081d81bcd999d3b
CMDR-Bill-Doors <Billyanghk@qq.com>
2025-01-31 09:48:56 +0000

    fuck mechanitor gizmo

---
+++ _Sources/Embergarden/Embergarden/HarmonyPatch/FuckMechanitorPatch.cs
@@ -0,0 +1,18 @@
+﻿using Verse;
+using HarmonyLib;
+using RimWorld;
+
+namespace Embergarden
+{
+    [StaticConstructorOnStartup]
+    [HarmonyPatch(typeof(CallBossgroupUtility), "BossgroupEverCallable")]
+    public static class FuckMechanitorPatch
+    {
+        public static bool Prefix(BossgroupDef def)
+        {
+            return !def.HasModExtension<FuckMechanitor>();
+        }
+    }
+
+    public class FuckMechanitor : DefModExtension { }
+}
