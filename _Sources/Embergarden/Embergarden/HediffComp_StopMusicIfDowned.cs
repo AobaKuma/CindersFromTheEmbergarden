cfa38040f78cae48fabf0075c8d8f416cbc5a1e3
CMDR-Bill-Doors <Billyanghk@qq.com>
2025-02-05 12:27:24 +0000

    stop music when downed

---
+++ _Sources/Embergarden/Embergarden/HediffComp_StopMusicIfDowned.cs
@@ -0,0 +1,32 @@
+﻿using Verse;
+
+namespace Embergarden
+{
+    public class HediffComp_StopMusicIfDowned : HediffComp_DownedAction
+    {
+        CompProperties_HediffStopMusicIfDowned Props => props as CompProperties_HediffStopMusicIfDowned;
+        public override void Notify_Downed()
+        {
+            var music = Find.MusicManagerPlay;
+            if (music.CurrentSong == Props.songDef) music.ForceTriggerNextSongOrSequence();
+            Pawn.health.RemoveHediff(parent);
+        }
+
+        public override void Notify_PawnDied(DamageInfo? dinfo, Hediff culprit = null)
+        {
+            var music = Find.MusicManagerPlay;
+            if (music.CurrentSong == Props.songDef) music.ForceTriggerNextSongOrSequence();
+            Pawn.health.RemoveHediff(parent);
+        }
+    }
+
+    public class CompProperties_HediffStopMusicIfDowned : HediffCompProperties
+    {
+        public CompProperties_HediffStopMusicIfDowned()
+        {
+            compClass = typeof(HediffComp_StopMusicIfDowned);
+        }
+
+        public SongDef songDef;
+    }
+}
---
+++ _Sources/Embergarden/Embergarden/Hediff_DownedAction.cs
@@ -0,0 +1,21 @@
+﻿using Verse;
+
+namespace Embergarden
+{
+    public class Hediff_DownedAction : HediffWithComps
+    {
+        public override void Notify_Downed()
+        {
+            base.Notify_Downed();
+            for (int num = comps.Count - 1; num >= 0; num--)
+            {
+                if (comps[num] is HediffComp_DownedAction c) c.Notify_Downed();
+            }
+        }
+    }
+
+    public abstract class HediffComp_DownedAction : HediffComp
+    {
+        public abstract void Notify_Downed();
+    }
+}
