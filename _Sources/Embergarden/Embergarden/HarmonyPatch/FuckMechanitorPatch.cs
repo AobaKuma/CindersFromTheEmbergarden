222845489f8c33c8099e963e1b06ee3fb405d516
CMDR-Bill-Doors <Billyanghk@qq.com>
2025-02-05 09:58:57 +0000

    fuck ideo apparel

--- _Sources/Embergarden/Embergarden/HarmonyPatch/FuckMechanitorPatch.cs
+++ _Sources/Embergarden/Embergarden/HarmonyPatch/FuckMechanitorPatch.cs
@@ -20,4 +20,20 @@ namespace Embergarden
     }
 
     public class FuckMechanitor : DefModExtension { }
+
+    [StaticConstructorOnStartup]
+    [HarmonyPatch(typeof(PawnGenerator), "GenerateOrRedressPawnInternal")]
+    static class FuckIdeoApparel
+    {
+        [HarmonyPrefix]
+        static void FuckingIdeoApparel(ref PawnGenerationRequest request)
+        {
+            if (request.KindDef.HasModExtension<ModExtension_NoIdeoApparel>())
+            {
+                request.ForceNoIdeoGear = true;
+            }
+        }
+    }
+
+    public class ModExtension_NoIdeoApparel : DefModExtension { };
 }
