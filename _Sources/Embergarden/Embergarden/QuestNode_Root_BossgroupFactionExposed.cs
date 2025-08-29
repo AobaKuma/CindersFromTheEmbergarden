cb6901571c04b7033e3648e2dea5681a8cfcbc60
Aoba <sakilomo@gmail.com>
2025-08-29 12:20:18 +0800

    Drop pod fix
    Drop pod fix

--- _Sources/Embergarden/Embergarden/QuestNode_Root_BossgroupFactionExposed.cs
+++ _Sources/Embergarden/Embergarden/QuestNode_Root_BossgroupFactionExposed.cs
@@ -94,7 +94,7 @@ namespace Embergarden
             Quest quest2 = quest;
             MapParent parent = map.Parent;
             string inSignal = text;
-            quest2.DropPods(parent, enumerable, null, null, null, null, false, useTradeDropSpot: false, joinPlayer: false, makePrisoners: false, inSignal, null, QuestPart.SignalListenMode.OngoingOnly, intVec, destroyItemsOnCleanup: true, dropAllInSamePod: false, allowFogged: false, faction);
+            quest2.DropPods(parent, enumerable, null, null, null, null, false, useTradeDropSpot: false, joinPlayer: false, makePrisoners: false, inSignal, null, QuestPart.SignalListenMode.OngoingOnly, intVec, destroyItemsOnCleanup: true, dropAllInSamePod: false, allowFogged: false, faction: faction);
 
             var BossLabelCap = bossgroupDef.boss.kindDef.LabelCap.Named("BOSSLABEL");
             var BossLabel = bossgroupDef.boss.kindDef.label.Named("BOSSLABEL");
